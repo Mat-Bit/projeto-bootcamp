2a36b203bb08bced768b5c6eb677909006df5f1c
joncruz96 <jonathan.luan@hotmail.com>
2018-07-23 21:15:32 -0300

    Add files via upload

--- queropizza.js
+++ queropizza.js
@@ -1,93 +1,83 @@
-var x=document.getElementById("erro");
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
 var map;
 var infowindow;
 
 function initialize() {
 
-    //var pyrmont = {lat: -26.2754221, lng: -48.8723681};
-    var pyrmont = getLocation();
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 15
-     });
-
-    infowindow = new google.maps.InfoWindow();
+  navigator.geolocation.getCurrentPosition(function(position) {
     
-    var request = {
-        location: pyrmont,
-        radius: '5000',
-        query: 'pizzaria'
+    var localAtual = { 
+      lat: position.coords.latitude, lng: position.coords.longitude 
     };
 
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: localAtual,
+      zoom: 15
+    });
+  
+  
+    infowindow = new google.maps.InfoWindow();
+    var request = {
+      location: localAtual,
+      radius: '500',
+      query: 'pizzaria'
+    };
+  
     service = new google.maps.places.PlacesService(map);
     service.textSearch(request, callback);
+
+    var marker=new google.maps.Marker({position:localAtual,map:map,title:"Você está Aqui!"});
+  
+  });  
 }
 
 function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
     }
-}
-
-function getLocation(){
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPosition,showError);
-    }
-    else{
-        x.innerHTML="Geolocalização não é suportada nesse browser.";
-    }
+  }
 }
 
 function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
- }
-
-function showPosition(position){
-  lat=position.coords.latitude;
-  lon=position.coords.longitude;
-  latlon=new google.maps.LatLng(lat, lon)
-  mapholder=document.getElementById('map')
-  mapholder.style.height='250px';
-  mapholder.style.width='500px';
- 
-  var myOptions={
-    center:latlon,zoom:14,
-    mapTypeId:google.maps.MapTypeId.ROADMAP,
-    mapTypeControl:false,
-    navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
-  };
-  var map=new google.maps.Map(document.getElementById("map"),myOptions);
-  var marker=new google.maps.Marker({position:latlon,map:map,title:"Você está Aqui!"});
-  
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
 
+function getLocation(){
+  if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(showPosition,showError);
+      //return;
+  }
+  x.innerHTML="Geolocalização não é suportada nesse browser."; 
+}
 
- 
-function showError(error){
-    switch(error.code){
-        case error.PERMISSION_DENIED:
-            x.innerHTML="Usuário rejeitou a solicitação de Geolocalização."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML="Localização indisponível."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML="O tempo da requisição expirou."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML="Algum erro desconhecido aconteceu."
-            break;
-    } 
+function showPosition(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+  latlon = new google.maps.LatLng(lat, lon)
+  mapholder = document.getElementById('map')
+  mapholder.style.height = '250px';
+  mapholder.style.width = '500px';
+
+  var myOptions = {
+    center: latlon, zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL }
+  };
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+  var marker = new google.maps.Marker({ position: latlon, map: map, title: "Você está Aqui!" });
+
 }
